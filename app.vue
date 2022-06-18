31c73770c2c76acb92c5dd714a46752b370655da
Vlad Starkovsky <vlad@starkovsky.ru>
2022-06-18 21:03:50 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useCounterStore } from './stores/counterStore';
+
+const counterStore = useCounterStore();
+
+</script>
+
+<template>
+  <div>
+    {{ counterStore.counter }}
+    <button @click="counterStore.increment">+</button>
+    <button @click="counterStore.decrement">-</button>
+  </div>
+</template>
